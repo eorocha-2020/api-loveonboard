2c5842dc198048489bf4cb7dfb616dabfaa3274b
Lucas Rocha <lucas.rocha99@hotmail.com>
2025-01-13 13:36:06 -0300

    ajuste preco

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ router.get("/", function (req, res, next) {
 router.post("/criar-pix", async function (req, res, next) {
   const data = req.body;
   const body = {
-    transaction_amount: data.payer.genero === "female" ? 80 : 90,
+    transaction_amount: data.payer.genero === "female" ? 80 : 100,
     description: data.description,
     payment_method_id: "pix",
     notification_url: process.env.NOTIFICATION_URL,
